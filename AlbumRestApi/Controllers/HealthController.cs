aee77d852012d59b5bf44579291394df7315beca
veyselboybay <veysel.boybay@outlook.com>
2023-03-22 16:28:13 -0400

    Added health controller

---
+++ AlbumRestApi/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlbumRestApi.Controllers
+{
+    [Route("health")]
+    public class HealthController : BaseController<HealthController>
+    {
+        public HealthController(ILogger<HealthController> logger) : base(logger)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Health()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
